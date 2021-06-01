9a1cbab0044131731725d4d2ebc8694629e74b1b
Cesar Whesly Segura <cesarw.segura@gmail.com>
2021-06-01 15:52:02 -0500

    +==

--- main.cpp
+++ main.cpp
@@ -17,11 +17,11 @@ bool isSunlight(cv::Mat image, int width, int height) {
         for (int x = 0; x < image.cols; x++)
             histogram[(int) image.at<uchar>(y, x)]++;
 
-    for (int i = 0; i < 256; i++)
-        std::cout << histogram[i] << " ";
+    //for (int i = 0; i < 256; i++)
+    //    std::cout << histogram[i] << " ";
 
-    int hist_w = 1920;
-    int hist_h = 1080;
+    int hist_w = width;
+    int hist_h = height;
     int bin_w = cvRound((double) hist_w / 256);
 
     cv::Mat histImage(hist_h, hist_w, CV_8UC1, cv::Scalar(255, 255, 255));
@@ -46,7 +46,7 @@ bool isSunlight(cv::Mat image, int width, int height) {
     int totPixeles = image.rows * image.cols;
     int numWhite = histogram[255];
     float percentageWhites = (numWhite * 100) / totPixeles;
-
+    std::cout << "Whites :" << percentageWhites << std::endl;
     if (percentageWhites > 9) {
         return true;
     } else {
@@ -72,7 +72,8 @@ int main() {
 		
 		sun_status = isSunlight(image,frame_width, frame_heigth);
 		cv::putText(image, std::to_string(sun_status), cv::Point2f(20,20), cv::FONT_HERSHEY_SIMPLEX, 2, cv::Scalar(0,255,0),2,8,false);
-		
+		std::cout << "Sun status: " << sun_status << std::endl;
+
 
 		cv::imshow("SunDetector",image);
 		char c = (char) cv::waitKey(25);
