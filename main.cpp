8aca5467f9db6734a49797e2d5676da32f7b144f
Cesar Whesly Segura <cesarw.segura@gmail.com>
2021-06-01 12:23:31 -0500

    +++

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
 
 int main() {
 	
